0f2dde0e0c7e22fc47fe44141ee30e9dcea8728c
Travis <jonesdt@us.ibm.com>
2019-01-11 04:00:07 -0600

    fix: Fixed package dependency tool to generate labeled repo files in '/logs/dependencies' for yum package collections

--- scripts/python/dependencies_delta_compare.py
+++ scripts/python/dependencies_delta_compare.py
@@ -10,12 +10,8 @@ cwd = os.getcwd()
 print("Current working directory is:", cwd) 
 os.system("ls -l /home/jonest/power-up/logs/dependencies")
 
-
 pre_file  = str(raw_input('Enter pre_install.txt File: '))
 post_file = str(raw_input('Enter post_install.txt File: '))
-
-os.system("cat {} | wc -l".format(pre_file))
-os.system("cat {} | wc -l".format(post_file))
 
 pre_dbfile  = open("{}".format(pre_file),'r')
 post_dbfile = open("{}".format(post_file),'r')
@@ -67,18 +63,82 @@ while format_menu == True:
       pkg_type_select = str(raw_input("Select Format Type: "
                                      "\n(3)Yum \n(4)PIP \n(5)Conda \n"))
       if pkg_type_select == '3':
-         os.system('touch formatted_{}; chmod 777 formatted_{}'
-                  .format(final_file,final_file))
-         yum_dbfile = open('yml_formatted_{}'.format(final_file),'w')
-         for j in final_pkg_list:
-            prefix_pkg_strip = j.split('.',1)[0] #splitting delta at '.' gather before
-            suffix_pkg_strip = j.split('.',1)[1] #splitting delta at '.' gather after 
-            print prefix_pkg_strip
-            print suffix_pkg_strip
-            new_value = os.popen("cat {} rpm_post_list.txt | grep {} "
-                                .format(post_file,pkg_strip)).read()
-            yum_dbfile.write('{}\n'.format(new_value))
-         os.system("sudo rm -rf formatted_{}".format(final_file))
+
+         dep_search = ['anaconda',
+                       'cuda-powerup',
+                       'powerai-powerup',
+                       'dependencies-powerup',
+                       'installed']
+
+         dep_files = ['anaconda_{}'.format(final_file),
+                      'cuda-powerup_{}'.format(final_file),
+                      'powerai-powerup_{}'.format(final_file),
+                      'dependencies-powerup_{}'.format(final_file),
+                      'installed_{}'.format(final_file)]
+
+         tmp_dep_files = ['tmp_anaconda_{}'.format(final_file),
+                          'tmp_cuda-powerup_{}'.format(final_file),
+                          'tmp_powerai-powerup_{}'.format(final_file),
+                          'tmp_dependencies-powerup_{}'.format(final_file),
+                          'tmp_installed_{}'.format(final_file)]
+
+
+         yum_conda_dbfile = open('anaconda_{}'
+                                .format(final_file),'w+')
+         yum_cuda_dbfile = open('cuda-powerup_{}'
+                               .format(final_file),'w+')
+         yum_powerai_dbfile = open('powerai-powerup_{}'
+                                  .format(final_file),'w+')
+         yum_dependencies_dbfile = open('dependencies-powerup_{}'
+                                       .format(final_file),'w+')
+         yum_installed_dbfile = open('installed_{}'
+                                    .format(final_file),'w+')
+
+         anaconda_list = []
+         cuda_powerup_list = []
+         powerai_powerup_list = []
+         dependencies_powerup_list = []
+         installed_list = []
+         
+
+         for search in dep_search:
+            dep_grep = os.popen("cat {} | grep {} > tmp_{}_{}"
+                               .format(final_file,search,search,final_file))
+
+         for line_a in open("{}".format(tmp_dep_files[0]),'r').readlines():
+            value_a = line_a.split('.',1)
+            anaconda_list.append(value_a[0])
+         for l in anaconda_list:    
+            new_value = os.popen("cat rpm_post_list.txt | grep {}".format(l)).read()
+            yum_conda_dbfile.write('{}\n'.format(new_value))
+               
+         for line_a in open("{}".format(tmp_dep_files[1]),'r').readlines():
+            value_a = line_a.split('.',1)
+            cuda_powerup_list.append(value_a[0])
+         for l in cuda_powerup_list:
+            new_value = os.popen("cat rpm_post_list.txt | grep {}".format(l)).read()
+            yum_cuda_dbfile.write('{}\n'.format(new_value))
+
+         for line_a in open("{}".format(tmp_dep_files[2]),'r').readlines():
+            value_a = line_a.split('.',1)
+            powerai_powerup_list.append(value_a[0])
+         for l in powerai_powerup_list:
+            new_value = os.popen("cat rpm_post_list.txt | grep {}".format(l)).read()
+            yum_powerai_dbfile.write('{}\n'.format(new_value))
+
+         for line_a in open("{}".format(tmp_dep_files[3]),'r').readlines():
+            value_a = line_a.split('.',1)
+            dependencies_powerup_list.append(value_a[0])
+         for l in dependencies_powerup_list:
+            new_value = os.popen("cat rpm_post_list.txt | grep {}".format(l)).read()
+            yum_dependencies_dbfile.write('{}\n'.format(new_value))
+
+         for line_a in open("{}".format(tmp_dep_files[4]),'r').readlines():
+            value_a = line_a.split('.',1)
+            installed_list.append(value_a[0])
+         for l in installed_list:
+            new_value = os.popen("cat rpm_post_list.txt | grep {}".format(l)).read()
+            yum_installed_dbfile.write('{}\n'.format(new_value))
          format_menu = False
 
       elif pkg_type_select == '4':
@@ -87,9 +147,7 @@ while format_menu == True:
             value_c = line_c.split()
             new_value = "{}=={}".format(value_c[0],value_c[1])
             pip_dbfile.write('{}\n'.format(new_value))
-            print new_value
          format_menu = False
-         print ("Done.")
 
       elif pkg_type_select == '5':
          conda_dbfile = open('{}'.format(final_file),'rb+')
@@ -98,12 +156,15 @@ while format_menu == True:
             new_value = ("{}-{}-{}.tar.bz2"
                         .format(value_d[0],value_d[1],value_d[2]))
             conda_dbfile.write('{}\n'.format(new_value))
-            print new_value
          format_menu = False
+      
       else:
          print ("Plese make valid selection")
    else:
       print ("Please select valid option")
+
+print ('Cleaning up Files...')
+os.system("sudo rm -rf tmp* {}".format(final_file))
 
 print ("Process Completed.")
 
