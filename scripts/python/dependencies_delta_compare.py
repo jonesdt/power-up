b6d51ab8c09b6e0d95277db2f4603fbdbf3eab3c
Travis <jonesdt@us.ibm.com>
2019-01-16 10:44:21 -0600

    fix: fixed removal of files

--- scripts/python/dependencies_delta_compare.py
+++ scripts/python/dependencies_delta_compare.py
@@ -176,6 +176,8 @@ while format_menu == True:
             version = value_a[1]
             new_value = "{}-{}.{}".format(prefix,version,suffix)
             yum_installed_dbfile.write('{}\n'.format(new_value))
+ 
+         os.system("sudo rm -rf tmp* {} ".format(final_file))
 
          format_menu = False
 
@@ -202,9 +204,6 @@ while format_menu == True:
          print ("Plese make valid selection")
    else:
       print ("Please select valid option")
-
-print ('Cleaning up Files...')
-os.system("sudo rm -rf tmp* {} ".format(final_file))
 
 print ("Process Completed.")
 
